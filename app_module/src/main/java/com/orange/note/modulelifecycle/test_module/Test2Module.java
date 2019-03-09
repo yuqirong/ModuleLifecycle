eed7bf6baade6a01ecaa29ee450d8855999b0ae6
俞其荣 <maomao@91chengguo.com>
2019-03-09 14:21:18 +0800

    rename module

--- app_module/src/main/java/com/orange/note/modulelifecycle/test_module/Test2Module.java
+++ app_module/src/main/java/com/orange/note/modulelifecycle/test_module/Test2Module.java
@@ -1,4 +1,4 @@
-package com.orange.note.modulelifecycle.demo;
+package com.orange.note.modulelifecycle.test_module;
 
 import android.util.Log;
 
@@ -7,23 +7,26 @@ import com.orange.note.modulelifecycle.annotation.Module;
 
 /**
  * @author maomao
- * @date 2019/3/7
+ * @date 2019/3/9
  */
 @Module
-public class TestModule implements BaseModule {
+public class Test2Module implements BaseModule {
 
     private static final String TAG = "TAG";
 
+    @Override
     public void onAppCreate() {
-        Log.i(TAG, " TestModule onAppCreate");
+        Log.i(TAG, "Test2Module onAppCreate ");
     }
 
+    @Override
     public void onAppStart() {
-        Log.i(TAG, "TestModule onAppStart");
+        Log.i(TAG, "Test2Module onAppStart ");
     }
 
+    @Override
     public void onAppStop() {
-        Log.i(TAG, "TestModule onAppStop");
+        Log.i(TAG, "Test2Module onAppStop ");
     }
 
 }
